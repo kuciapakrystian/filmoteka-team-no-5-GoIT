8fc4103da59497043becfa589e150007e6052342
kuciapakrystian <kuciapakrystian@outlook.com>
2024-03-06 15:32:53 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './sass/main.scss';
+'use strict';
+
+import './sass/components/_button.scss';
+import './js/cards-home';
+import './js/fetch';
+import './js/modal-card';
+import './js/search-form';
+
+import './js/login';
+import './js/login-modal';
+import './js/day-night';
+
+import './js/localStorage';
+/* import './js/library';
+import './js/library-render'; */
+import './js/newLibrary'
+
+import './js/localStorage';
+import './js/students-modal';
+import './js/refreshPage';
+import './js/spinner';
+import './js/refreshrendering';
+
+import './js/newpagin';
+import './js/to-top';
