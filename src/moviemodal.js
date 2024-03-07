72748ee7e43945970e51f8aa4dac6f9d87642ef5
Kodiisek <kodisekk@gmail.com>
2024-03-07 19:10:59 +0100

    upd

---
+++ src/moviemodal.js
@@ -0,0 +1,17 @@
+fetch('https://api.themoviedb.org/3/movie/550?api_key=4055b791708338774332fbeb5d716522')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Wyświetlenie danych w konsoli w celu zrozumienia ich struktury i dostępnych pól
+        
+        // Aktualizacja danych w modalu
+        document.querySelector('.movie-poster').src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+        document.querySelector('.votes').textContent = data.vote_count;
+        document.querySelector('.popularity').textContent = data.popularity;
+        document.querySelector('.original-title').textContent = data.original_title;
+        document.querySelector('.genre').textContent = data.genres.map(genre => genre.name).join(', ');
+        document.querySelector('.about').textContent = data.overview;
+
+        // Pokazanie modala
+        document.querySelector('.modal').style.display = 'block';
+    })
+    .catch(error => console.error('Error fetching data:', error));
