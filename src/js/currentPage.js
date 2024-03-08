89d99332fa818cb601422fa7e59fdfd08351ea7c
pawelek <k.pawelek@vp.pl>
2024-03-08 13:15:34 +0100

    Removal of supporting comments

--- src/js/currentPage.js
+++ src/js/currentPage.js
@@ -7,21 +7,16 @@ libraryBodyContainer.classList.add('hidden');
 
 const link = document.querySelector('a');
 document.addEventListener('DOMContentLoaded', function () {
-  // Pobierz wszystkie linki, które mają klasę js-library lub page-home
   const links = document.querySelectorAll('.js-library, .page-home');
 
-  // Dodaj nasłuchiwanie zdarzenia kliknięcia dla każdego linku
   links.forEach(function (link) {
     link.addEventListener('click', function (event) {
-      // Zapobiegnij domyślnemu zachowaniu przeglądarki (przeładowaniu strony)
       event.preventDefault();
 
-      // Pobierz identyfikator strony do wyświetlenia na podstawie klasy linku
       const pageId = this.classList.contains('js-library')
         ? 'library-body-container'
         : 'home-body-container';
 
-      // Wywołaj funkcję showPage(), przekazując identyfikator strony
       showPage(pageId);
     });
   });
@@ -30,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
 function showPage(pageId) {
   const pages = document.querySelectorAll('.page');
   pages.forEach(function (page) {
-    // Ukryj wszystkie strony, które nie są aktualnie wyświetlane
     if (page.id !== pageId) {
       page.classList.add('hidden');
     }
@@ -39,7 +33,6 @@ function showPage(pageId) {
   const selectedPage = document.getElementById(pageId);
   selectedPage.classList.remove('hidden');
 
-  // Ukryj library-body-container, jeśli home-body-container jest aktywny
   const libraryBodyContainer = document.getElementById('library-body-container');
   const homeBodyContainer = document.getElementById('home-body-container');
 
