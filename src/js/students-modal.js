8f029bf8e018a0232a9b06944bbe9c459fc24713
kuciapakrystian <kuciapakrystian@outlook.com>
2024-03-16 11:12:56 +0100

    update

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -1,31 +1,26 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('body'),
+   openModalBtn: document.querySelector('[data-modal-open]'),
+   closeModalBtn: document.querySelector('[data-modal-close]'),
+   modal: document.querySelector('[data-modal]'),
+   body: document.querySelector('body'),
   };
   refs.modal.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal(e) {
-    if (Object.values(refs).includes(e.target)) {
-      refs.modal.classList.toggle('is-hidden');
-      onBodyToggle();
-    }
+   if (Object.values(refs).includes(e.target)) {
+    refs.modal.classList.toggle('is-hidden');
+    onBodyToggle();
+   }
   }
-
   window.addEventListener('keydown', closeModalByEscape);
-
   function closeModalByEscape(e) {
-    if (e.code === 'Escape' && !refs.modal.classList.value.includes('is-hidden')) {
-      refs.modal.classList.toggle('is-hidden');
-      onBodyToggle();
-    }
+   if (e.code === 'Escape' && !refs.modal.classList.value.includes('is-hidden')) {
+    refs.modal.classList.toggle('is-hidden');
+    onBodyToggle();
+   }
   }
-
   function onBodyToggle() {
-    refs.body.classList.toggle('team-open');
+   refs.body.classList.toggle('team-open');
   }
-})();
-
+ })();
